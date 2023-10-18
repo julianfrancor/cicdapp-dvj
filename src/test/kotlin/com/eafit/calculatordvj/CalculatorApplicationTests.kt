f49fa67536053151d504b74979d992fe05b9a3b1
Julian <julianfrancor@gmail.com>
2023-10-18 00:43:47 -0500

    [FTR] Update innitial scaffold

---
+++ src/test/kotlin/com/eafit/calculatordvj/CalculatorApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.eafit.calculatordvj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
