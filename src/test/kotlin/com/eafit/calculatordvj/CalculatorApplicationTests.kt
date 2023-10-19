583c7dcc03eb1a33085d7185e56089c30a46a394
Julian <julianfrancor@gmail.com>
2023-10-18 20:57:44 -0500

    [FTR] Add integration tests

--- src/test/kotlin/com/eafit/calculatordvj/CalculatorApplicationTests.kt
+++ src/test/kotlin/com/eafit/calculatordvj/CalculatorApplicationTests.kt
@@ -3,66 +3,68 @@ package com.eafit.calculatordvj
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.ResponseEntity
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CalculatorApplicationTests {
 
+    @LocalServerPort
+    private var port: Int = 0
+
     @Autowired
-    private lateinit var mockMvc: MockMvc
+    private lateinit var restTemplate: TestRestTemplate
 
     @Test
     fun contextLoads() {
     }
 
+    private fun getRequest(url: String): String {
+        val fullUrl = "http://localhost:$port$url"
+        val response: ResponseEntity<String> = restTemplate.getForEntity(fullUrl, String::class.java)
+        return response.body ?: ""
+    }
+
     @Test
     fun testIndexWithNameParam() {
-        mockMvc.perform(get("/?name=John"))
-            .andExpect(status().isOk)
-            .andExpect(content().string("Hello, John!"))
+        val response = getRequest("/?name=John")
+        assert(response.contains("Hello, John!"))
     }
 
     @Test
     fun testHelloWithNameParam() {
-        mockMvc.perform(get("/hello?name=Jane"))
-            .andExpect(status().isOk)
-            .andExpect(content().string("Hello Jane!"))
+        val response = getRequest("/hello?name=Jane")
+        assert(response.contains("Hello Jane!"))
     }
 
     @Test
     fun testHelloWithoutNameParam() {
-        mockMvc.perform(get("/hello"))
-            .andExpect(status().isOk)
-            .andExpect(content().string("Hello World!"))
+        val response = getRequest("/hello")
+        assert(response.contains("Hello World!"))
     }
 
     @Test
     fun testSquare() {
-        mockMvc.perform(get("/square?firstNumber=5"))
-            .andExpect(status().isOk)
-            .andExpect(content().string("The square of 5.0 is 25.0"))
+        val response = getRequest("/square?firstNumber=5")
+        assert(response.contains("The square of 5.0 is 25.0"))
     }
 
     @Test
     fun testCube() {
-        mockMvc.perform(get("/cube?firstNumber=3"))
-            .andExpect(status().isOk)
-            .andExpect(content().string("The cube of 3.0 is 27.0"))
+        val response = getRequest("/cube?firstNumber=3")
+        assert(response.contains("The cube of 3.0 is 27.0"))
     }
 
     @Test
     fun testSum() {
-        mockMvc.perform(get("/sum?firstNumber=10&secondNumber=7"))
-            .andExpect(status().isOk)
-            .andExpect(content().string("The sum of 10.0 and 7.0 is 17.0"))
+        val response = getRequest("/sum?firstNumber=10&secondNumber=7")
+        assert(response.contains("The sum of 10.0 and 7.0 is 17.0"))
     }
 
     @Test
     fun testSubtract() {
-        mockMvc.perform(get("/subtract?firstNumber=15&secondNumber=8"))
-            .andExpect(status().isOk)
-            .andExpect(content().string("The difference of 15.0 and 8.0 is 7.0"))
+        val response = getRequest("/subtract?firstNumber=15&secondNumber=8")
+        assert(response.contains("The difference of 15.0 and 8.0 is 7.0"))
     }
 }
